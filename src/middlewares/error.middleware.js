d0b7ad48efc5312aa65ca0e91345095aa077748a
Aarav8400 <55765837+Aarav8400@users.noreply.github.com>
2024-03-30 12:55:05 -0700

    trying hit and trail

--- src/middlewares/error.middleware.js
+++ src/middlewares/error.middleware.js
@@ -6,30 +6,26 @@ export const errorMiddleware = (err, req, res, next) => {
 
   if (err.code === 11000) {
     const message = `Duplicate ${Object.keys(err.keyValue)} Entered`,
-      err = new ApiError(400, message);
+      err = new ErrorHandler(message, 400);
   }
   if (err.name === "JsonWebTokenError") {
     const message = `Json Web Token is invalid, Try again!`;
-    err = new ApiError(400, message);
+    err = new ErrorHandler(message, 400);
   }
   if (err.name === "TokenExpiredError") {
     const message = `Json Web Token is expired, Try again!`;
-    err = new ApiError(400, message);
+    err = new ErrorHandler(message, 400);
   }
   if (err.name === "CastError") {
     const message = `Invalid ${err.path}`,
-      err = new ApiError(400, message);
+      err = new ErrorHandler(message, 400);
   }
 
-  let errorMessage;
-
-  if (err.errors && err.errors.length !== 0) {
-    // If specific errors exist
-    errorMessage = Object.values(err.errors).map((error) => error.message);
-  } else {
-    // If there are no specific errors
-    errorMessage = [err.message];
-  }
+  const errorMessage = err.errors
+    ? Object.values(err.errors)
+        .map((error) => error.message)
+        .join(" ")
+    : err.message;
 
   return res.status(err.statusCode).json({
     success: false,
