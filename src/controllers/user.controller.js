feb9fa643a6061c14d6e23c403f7136d61314426
Aarav8400 <55765837+Aarav8400@users.noreply.github.com>
2024-03-31 01:32:54 -0700

    nothing just commit

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -4,7 +4,6 @@ import { asyncHandler } from "../utils/asyncHandler.js";
 import { User } from "../models/user.model.js";
 import { generateJwtToken } from "../utils/jwtToken.js";
 import { uploadOnCloudinary } from "../utils/cloudinary.js";
-
 const patientRegister = asyncHandler(async (req, res) => {
   const { firstName, lastName, email, phone, password, gender, dob, role } =
     req.body;
